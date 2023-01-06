510b75c498ffdfa974178957a4f68e078f5d855e
root <root@localhost.localdomain>
2023-01-06 11:33:22 +0800

    first

---
+++ internal/miniblog/miniblog.go
@@ -0,0 +1,84 @@
+// Copyright 2022 Innkeeper Belm(孔令飞) <nosbelm@qq.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file. The original repo for
+// this file is https://github.com/marmotedu/miniblog.
+
+package miniblog
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"github.com/marmotedu/miniblog/internal/pkg/log"
+	"github.com/marmotedu/miniblog/pkg/version/verflag"
+)
+
+var cfgFile string
+
+// NewMiniBlogCommand 创建一个 *cobra.Command 对象. 之后，可以使用 Command 对象的 Execute 方法来启动应用程序.
+func NewMiniBlogCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		// 指定命令的名字，该名字会出现在帮助信息中
+		Use: "miniblog",
+		// 命令的简短描述
+		Short: "A good Go practical project",
+		// 命令的详细描述
+		Long: `A good Go practical project, used to create user with basic information.
+
+Find more miniblog information at:
+	https://github.com/marmotedu/miniblog#readme`,
+
+		// 命令出错时，不打印帮助信息。不需要打印帮助信息，设置为 true 可以保持命令出错时一眼就能看到错误信息
+		SilenceUsage: true,
+		// 指定调用 cmd.Execute() 时，执行的 Run 函数，函数执行失败会返回错误信息
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// 如果 `--version=true`，则打印版本并退出
+			verflag.PrintAndExitIfRequested()
+
+			// 初始化日志
+			log.Init(logOptions())
+			defer log.Sync() // Sync 将缓存中的日志刷新到磁盘文件中
+
+			return run()
+		},
+		// 这里设置命令运行时，不需要指定命令行参数
+		Args: func(cmd *cobra.Command, args []string) error {
+			for _, arg := range args {
+				if len(arg) > 0 {
+					return fmt.Errorf("%q does not take any arguments, got %q", cmd.CommandPath(), args)
+				}
+			}
+
+			return nil
+		},
+	}
+
+	// 以下设置，使得 initConfig 函数在每个命令运行时都会被调用以读取配置
+	cobra.OnInitialize(initConfig)
+
+	// 在这里您将定义标志和配置设置。
+
+	// Cobra 支持持久性标志(PersistentFlag)，该标志可用于它所分配的命令以及该命令下的每个子命令
+	cmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "The path to the miniblog configuration file. Empty string for no configuration file.")
+
+	// Cobra 也支持本地标志，本地标志只能在其所绑定的命令上使用
+	cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	// 添加 --version 标志
+	verflag.AddFlags(cmd.PersistentFlags())
+
+	return cmd
+}
+
+// run 函数是实际的业务代码入口函数.
+func run() error {
+	// 打印所有的配置项及其值
+	settings, _ := json.Marshal(viper.AllSettings())
+	log.Infow(string(settings))
+	// 打印 db -> username 配置项的值
+	log.Infow(viper.GetString("db.username"))
+	return nil
+}
